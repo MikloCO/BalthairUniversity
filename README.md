2e4fefdd882d522a3f9828aa6146f7fd8e40da66
MikloCO <37591806+MikloCO@users.noreply.github.com>
2021-05-06 21:46:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BalthairUniversity
+
+Developed with Unreal Engine V. 4.22.3 
---
+++ Source/BalthairUniversity/BalthairUniversityGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BalthairUniversityGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BALTHAIRUNIVERSITY_API ABalthairUniversityGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
